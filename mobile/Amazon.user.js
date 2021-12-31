79d0d95183554a6b098d5ad4b3861949f043f716
Alucrud <96896509+Alucrud@users.noreply.github.com>
2021-12-30 21:03:49 -0600

    Update Amazon.user.js

--- mobile/Amazon.user.js
+++ mobile/Amazon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon
 // @namespace    https://github.com/Alucrud/TamperMonkeyCrap/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.amazon.com/*/dp/*
