3ac261e98a311c6ac83f84ccaeb07980d3fe8a22
Alucrud <96896509+Alucrud@users.noreply.github.com>
2021-12-30 20:08:28 -0600

    Create 1337to.js

---
+++ 1337to.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         1337x.to
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  1337x.to
+// @author       Alucrud
+// @include      https://www.1337x.to/*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+// Returns a Promise that resolves after "ms" Milliseconds
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+async function load () { // We need to wrap the loop into an async function for this to work
+  for (var i = 0; i < 1000; i++) {
+      $(".box-info").remove(); //remove network status box
+      $(".banner-box.movie").remove(); //remove scrolling movie torrents
+      $("h3:contains('Most Popular Torrents this week')").closest("div").remove(); //remove top torrents
+      $("a[title='Most Popular Foreign Movies This Week']").closest("div").remove(); //remove foreign movie torrents
+      $("a[title='Most Popular Applications This Week']").closest("div").remove(); //remove application torrents
+      $("a[title='Most Popular Music This Week']").closest("div").remove(); //remove Music torrents
+
+      //loop stuff
+      if(i<11) //runs 10 times quick, then slows down
+      {
+          await timer(500); // then the created Promise can be awaited
+      }
+      else
+      {
+          await timer(2000); // then the created Promise can be awaited
+      }
+  }
+}
+
+load();
