96934bc075926b88cbfc77622f76b91332d62239
Alucrud <96896509+Alucrud@users.noreply.github.com>
2022-03-06 13:09:01 -0600

    Update D2 Runewords - Close Nag.user.js

--- mobile/D2 Runewords - Close Nag.user.js
+++ mobile/D2 Runewords - Close Nag.user.js
@@ -6,6 +6,7 @@
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @noframes
 // @run-at document-idle
+// @updateURL https://raw.githubusercontent.com/Alucrud/TamperMonkeyCrap/main/mobile/D2%20Runewords%20-%20Close%20Nag.user.js
 // ==/UserScript==
 
 // #community-note .note
