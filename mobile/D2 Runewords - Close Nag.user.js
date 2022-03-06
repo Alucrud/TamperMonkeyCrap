1289e58fc802d36119e7ed32a4915bebb4038935
Alucrud <96896509+Alucrud@users.noreply.github.com>
2022-03-06 13:06:52 -0600

    Create D2 Runewords - Close Nag.user.js

---
+++ mobile/D2 Runewords - Close Nag.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name D2 Runewords - Close Nag
+// @version 0.0.1
+// @match https://d2runewords.com/*
+// @grant none
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @noframes
+// @run-at document-idle
+// ==/UserScript==
+
+// #community-note .note
+$("#community-note").remove();
