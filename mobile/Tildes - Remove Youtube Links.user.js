ae98e4d5baf43d02af4bedcefc47e79a79c8033f
Alucrud <96896509+Alucrud@users.noreply.github.com>
2023-07-17 14:23:34 -0500

    Update Tildes - Remove Youtube Links.user.js

--- mobile/Tildes - Remove Youtube Links.user.js
+++ mobile/Tildes - Remove Youtube Links.user.js
@@ -3,11 +3,12 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://tildes.net/*
 // @grant       GM_addStyle
-// @version     1.3
+// @version     1.31
 // @author      -
 // @description Hides any submitted links that point to youtube.
 // @updateURL   https://github.com/Alucrud/TamperMonkeyCrap/raw/main/mobile/Tildes%20-%20Remove%20Youtube%20Links.user.js
 // @icon        https://tildes.net/favicon.ico
+// @inject-into page
 // ==/UserScript==
 
 
