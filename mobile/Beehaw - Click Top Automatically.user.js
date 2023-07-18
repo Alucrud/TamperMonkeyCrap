54b690ab75be05cbd47400453aa8c86f084908bf
Alucrud <96896509+Alucrud@users.noreply.github.com>
2023-07-17 22:35:14 -0500

    Create Beehaw - Click Top Automatically.user.js

---
+++ mobile/Beehaw - Click Top Automatically.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Beehaw - Click 'Top' Automatically
+// @namespace   Violentmonkey Scripts
+// @include       *://beehaw.org/post/*
+// @grant       none
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @version     1.0
+// @author      Alucrud
+// @description When opening a post, click "top" automatically
+// @updateURL   https://github.com/Alucrud/TamperMonkeyCrap/raw/main/mobile/Beehaw%20-%20Click%20Top%20Automatically.user.js
+// @icon        https://beehaw.org/favicon.ico
+// ==/UserScript==
+
+function clicky () {
+  document.querySelectorAll("input[value='Top']").forEach(el=>el.click());
+}
+
+waitForKeyElements ("#sidebarInfo", clicky);
