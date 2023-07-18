6d272c04a6a07137c15fb4c809e51719dcbea047
Alucrud <96896509+Alucrud@users.noreply.github.com>
2023-07-18 12:08:20 -0500

    Update Beehaw - Click Top Automatically.user.js

--- mobile/Beehaw - Click Top Automatically.user.js
+++ mobile/Beehaw - Click Top Automatically.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        Beehaw - Click 'Top' Automatically
 // @namespace   Violentmonkey Scripts
-// @match       *://beehaw.org/post/*
+// @match       *://beehaw.org/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @version     1.1
+// @version     1.11
 // @author      Alucrud
 // @description When opening a post, click "top" automatically
 // @updateURL   https://github.com/Alucrud/TamperMonkeyCrap/raw/main/mobile/Beehaw%20-%20Click%20Top%20Automatically.user.js
