f75957cd70a74b671ba128a63cf373661415a740
Alucrud <96896509+Alucrud@users.noreply.github.com>
2024-04-04 00:34:06 -0500

    Update Imgur.user.js

--- mobile/Imgur.user.js
+++ mobile/Imgur.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name Open Imgur Links in New Tab
-// @version 0.0.1
+// @name Imgur
+// @version 0.0.2
 // @include *://*imgur*/*
 // @grant none
 // @require http://code.jquery.com/jquery-3.4.1.min.js
@@ -9,10 +9,40 @@
 // @run-at document-idle
 // ==/UserScript==
 
+//// Replace contains with a case-insensitive version
+jQuery.expr[':'].contains = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
 
-function newTab (jNode) {
-    //***** YOUR CODE HERE *****
-    jNode.attr('target', '_blank');
-}
+////Variables
+//Remove
+var removeList = `
+  memes
+  dump
+  wholesome
+  politics
+  political
+  shitpost
+  cakeday
+`;
+
+
+
+waitForKeyElements (".TagPill", filter);
 
 waitForKeyElements ("a", newTab);
+
+function newTab (jNode) {
+  jNode.attr('target', '_blank');
+};
+
+function filter (jNode) {
+  $.each(removeList.split(/\r?\n/), function(){
+      var rep = this.trim();
+      if (rep.length > 0){
+        $(".TagPill:contains('" + rep + "')").closest(".Post-item").parent().hide();
+      };
+    });
+};
+
